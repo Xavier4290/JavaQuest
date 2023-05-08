f989a018b2883c9c705aef022406c5c127203dd5
Xavierzn <131989419+Xavier4290@users.noreply.github.com>
2023-05-08 18:21:49 -0300

    .
    .

--- JavaQuest/README.md
+++
@@ -1,2 +0,0 @@
-"# JavaQuest" 
-"# JavaQuest" 
---
+++ src/Classes/Ladino.java
@@ -0,0 +1,28 @@
+package Classes;
+
+import java.util.Scanner;
+
+public class Ladino extends Jogador{
+	
+	private int vida = 85;
+	private int armadura = 80;
+	private int stamina = 120;
+	private int dano = 8;
+	private String nome_classe = "ladino";
+	
+	public Ladino() {
+	
+	}
+	public Ladino(Scanner nome) {
+		super(nome);
+	}
+	
+	@Override
+	public void mostrarAtri() {
+		System.out.println("Vida: "+this.vida +"\n"
+				+"Armadura: "+this.armadura+"\n"
+				+"Stamina: " +this.stamina+"\n"
+				+"Dano: "+this.dano+"\n");
+	}
+
+}
